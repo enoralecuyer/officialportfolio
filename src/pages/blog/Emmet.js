74e1d4fb3a082791bb5ab76f2117a37f69d89687
enoralecuyer <hey.enora@gmail.com>
2021-05-13 18:53:40 -0700

    Simplified my most recent blog article

--- src/pages/blog/Emmet.js
+++ src/pages/blog/Emmet.js
@@ -15,14 +15,14 @@ function Blog() {
           </h1>
           <p>May 13, 2021</p>
           <p>
-            Emmet shortcuts allow me to write my code faster in Visual Studio Code, and these are the ones I use the most:
+            Emmet shortcuts allow me to code faster in Visual Studio Code. This is my top 10!
           </p>
           <p className="title-with-border">
             <strong>1. Boilerplate HTML (The '!' at the very beginning of a HTML page)</strong>
           </p>
-          <p>Just type:</p>
+          <p>Type:</p>
           <pre>{`!`}</pre>
-          <p>And it will print:</p>
+          <p>Result:</p>
           <pre>{`<!DOCTYPE html>
             <html lang="en">
             <head>
@@ -40,36 +40,36 @@ function Blog() {
           <p className="title-with-border">
             <strong>2. Import the CSS Stylesheet</strong>
           </p>
-          <p>Just type:</p>
+          <p>Type:</p>
           <pre>{` link:css `}</pre>
-          <p>And it will print:</p>
+          <p>Result:</p>
           <pre>{`<link rel="stylesheet" href="style.css">`}
           </pre>
 
           <p className="title-with-border">
             <strong>3. Write tags</strong>
           </p>
-          <p>If you want to create a paragraph, a h1, a section or any tag:</p>
+          <p>Type:</p>
           <pre>{`p`}</pre>
-          <p>And it will print your full tag, while placing your cursor exactly in the middle:</p>
+          <p>Result:</p>
           <pre>{`<p></p>`}
           </pre>
 
           <p className="title-with-border">
             <strong>5. Lorem Ipsum</strong>
           </p>
-          <p>Just type:</p>
+          <p>Type:</p>
           <pre>{` lorem50 `}</pre>
-          <p>And it will print the first 50 words from the lorem ipsum:</p>
+          <p>Result:</p>
           <pre>{`Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident voluptatibus numquam aspernatur necessitatibus ab odit ipsam delectus vel sapiente architecto dolores cum velit quis et magni, aperiam saepe mollitia nobis suscipit temporibus illo eius voluptatem nulla! Eligendi molestiae unde earum impedit sunt deleniti quaerat error? Veritatis deserunt tenetur eos laborum?`}
           </pre>
 
           <p className="title-with-border">
             <strong>6. Paragraphs of lorem ipsum</strong>
           </p>
-          <p>Create two paragraphs and populate them with lorem ipsum:</p>
+          <p>Type:</p>
           <pre>{` (p>lorem5)*2 `}</pre>
-          <p>And it will print:</p>
+          <p>Result:</p>
           <pre>{`<p>Lorem ipsum dolor sit amet.</p>
            <p>Non sapiente accusamus at eum?</p>`}
           </pre>
@@ -77,38 +77,38 @@ function Blog() {
           <p className="title-with-border">
             <strong>7. Div with a class</strong>
           </p>
-          <p>To create a div with the class "hero-class", just type:</p>
+          <p>Type:</p>
           <pre>{`.hero-class`}</pre>
-          <p>And it will print:</p>
+          <p>Result:</p>
           <pre>{`<div class="hero-class"></div>`}
           </pre>
 
           <p className="title-with-border">
             <strong>8. Div with an id</strong>
           </p>
-          <p>Similarly, to create a div with an id of "hero-id", just type:</p>
+          <p>Type:</p>
           <pre>{`#hero-id`}</pre>
-          <p>And it will print:</p>
+          <p>Result:</p>
           <pre>{`<div id="hero-id"></div>`}
           </pre>
 
           <p className="title-with-border">
-            <strong>9. Child and Parent Div</strong>
+            <strong>9. Parent div and child div</strong>
           </p>
-          <p>To create a child div inside of a parent div:</p>
+          <p>Type:</p>
           <pre>{`section>p`}</pre>
-          <p>And it will print:</p>
+          <p>Result:</p>
           <pre>{` <section>
                       <p></p>
                   </section>`}
           </pre>
 
           <p className="title-with-border">
-            <strong>10. Combine it all! </strong>
+            <strong>10. Combine them all! </strong>
           </p>
-          <p>To create an unordered list of 10 items, each containing 5 words of the lorem ipsum: </p>
+          <p>Type: </p>
           <pre>{`ul.list>(li>lorem5)*10`}</pre>
-          <p>And it will print:</p>
+          <p>Result:</p>
           <pre>{` <ul class="list">
                         <li>Lorem ipsum dolor sit amet.</li>
                         <li>Inventore culpa vel quod labore!</li>
