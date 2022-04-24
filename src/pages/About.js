58e26950806b7118e09b9db9d4e33f87bfd242a5
enoralecuyer <48727972+enoralecuyer@users.noreply.github.com>
2022-04-24 09:26:38 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -89,7 +89,7 @@ function About() {
             <p className="pt-3">
               <strong>Get in touch: </strong>
               <a
-                href="https://drive.google.com/file/d/1eQky9y91-gGIz2dVGKgIYSH53G3KE9xZ/view?usp=sharing"
+                href="https://drive.google.com/file/d/1E-zcIcL7TAbZ3AgWZjrPbem0AkGksS4E/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
